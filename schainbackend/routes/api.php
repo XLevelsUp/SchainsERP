4d010ce43394d0b05a0a079cd442ff12853259d5
azartechnet <azartechnet@gmail.com>
2026-07-30 21:45:56 +0530

    Update api.php

--- schainbackend/routes/api.php
+++ schainbackend/routes/api.php
@@ -1,74 +1,161 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserDetailController;
+
 use App\Http\Controllers\Api\ItemController;
 use App\Http\Controllers\Api\FitemBoxController;
+use App\Http\Controllers\Api\RoleController;
+use App\Http\Controllers\Api\UserDetailController;
+use App\Http\Controllers\Api\CustomerTouchController;
 use App\Http\Controllers\Api\UsersItemsMappingController;
 use App\Http\Controllers\Api\HeadEmployeeMappingController;
 use App\Http\Controllers\Api\CashHeadEmployeeMappingController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CashTxnDetailController;
-use App\Http\Controllers\Api\RoleController;
-use App\Http\Controllers\Api\CustomerTouchController;
 
 
+/*
+|--------------------------------------------------------------------------
+| API VERSION 1
+|--------------------------------------------------------------------------
+*/
+
+Route::prefix('v1')->group(function () {
+
+    /*
+    |--------------------------------------------------------------------------
+    | Items
+    |--------------------------------------------------------------------------
+    */
+
+    Route::apiResource(
+        'items',
+        ItemController::class
+    );
 
 
+    /*
+    |--------------------------------------------------------------------------
+    | Fitem Boxes
+    |--------------------------------------------------------------------------
+    */
 
-// Items
-Route::apiResource('items', ItemController::class);
+    Route::apiResource(
+        'fitem-boxes',
+        FitemBoxController::class
+    );
 
-// Fitem Boxes
-Route::apiResource('fitem-boxes', FitemBoxController::class);
 
-//roles
-Route::apiResource('roles', RoleController::class);
-// User Details
-Route::apiResource(
-    'user-details',
-    UserDetailController::class
-);
+    /*
+    |--------------------------------------------------------------------------
+    | Roles
+    |--------------------------------------------------------------------------
+    */
 
-//customer_touch
-Route::apiResource(
-    'customer-touch',
-    CustomerTouchController::class
-);
+    Route::apiResource(
+        'roles',
+        RoleController::class
+    );
 
-// Users Items Mappings
-Route::apiResource(
-    'users-items-mappings',
-    UsersItemsMappingController::class
-);
 
-// Head Employee Mappings
-Route::apiResource(
-    'head-employee-mappings',
-    HeadEmployeeMappingController::class
-);
+    /*
+    |--------------------------------------------------------------------------
+    | User Details
+    |--------------------------------------------------------------------------
+    */
 
-// Cash Head Employee Mappings
-Route::apiResource(
-    'cash-head-employee-mappings',
-    CashHeadEmployeeMappingController::class
-);
+    Route::apiResource(
+        'user-details',
+        UserDetailController::class
+    );
 
-//Auth_login
-Route::post('/login', [AuthController::class, 'login']);
 
-//cashtxndetailcontroller
-Route::apiResource(
-    'cash-txn-details',
-    CashTxnDetailController::class
-);
+    /*
+    |--------------------------------------------------------------------------
+    | Customer Touch
+    |--------------------------------------------------------------------------
+    */
 
-Route::post(
-    'cash-txn-details/{id}/images',
-    [CashTxnDetailController::class, 'addImages']
-);
+    Route::apiResource(
+        'customer-touch',
+        CustomerTouchController::class
+    );
 
-Route::delete(
-    'cash-txn-images/{imageId}',
-    [CashTxnDetailController::class, 'deleteImage']
-);
+
+    /*
+    |--------------------------------------------------------------------------
+    | Users Items Mappings
+    |--------------------------------------------------------------------------
+    */
+
+    Route::apiResource(
+        'users-items-mappings',
+        UsersItemsMappingController::class
+    );
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Head Employee Mappings
+    |--------------------------------------------------------------------------
+    */
+
+    Route::apiResource(
+        'head-employee-mappings',
+        HeadEmployeeMappingController::class
+    );
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cash Head Employee Mappings
+    |--------------------------------------------------------------------------
+    */
+
+    Route::apiResource(
+        'cash-head-employee-mappings',
+        CashHeadEmployeeMappingController::class
+    );
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication
+    |--------------------------------------------------------------------------
+    */
+
+    Route::post(
+        '/login',
+        [AuthController::class, 'login']
+    );
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cash Transaction Details
+    |--------------------------------------------------------------------------
+    */
+
+    Route::apiResource(
+        'cash-txn-details',
+        CashTxnDetailController::class
+    );
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cash Transaction Images
+    |--------------------------------------------------------------------------
+    */
+
+    Route::post(
+        'cash-txn-details/{id}/images',
+        [CashTxnDetailController::class, 'addImages']
+    );
+
+
+    Route::delete(
+        'cash-txn-images/{imageId}',
+        [CashTxnDetailController::class, 'deleteImage']
+    );
+});
